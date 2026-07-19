e676bd124b45ba97f4ae3c35bc221353a27d9992
FoxExe <fox.axon@gmail.com>
2026-07-20 00:29:20 +0500

    Fix CYRILLIC_SUPPORT for some displays (UTF8 already converted inside UTTask.cpp)

--- src/helpers/ui/ST7735Display.cpp
+++ src/helpers/ui/ST7735Display.cpp
@@ -90,8 +90,6 @@ void ST7735Display::startFrame(Color bkg) {
   display.setTextSize(1);      // This one affects size of Please wait... message
 #ifdef CYRILLIC_SUPPORT
   display.setFont(&glcdfont6x8);
-#else
-  display.cp437(true);         // Use full 256 char 'Code Page 437' font
 #endif
 }
 
@@ -142,11 +140,6 @@ void ST7735Display::setCursor(int x, int y) {
 }
 
 void ST7735Display::print(const char* str) {
-#ifdef CYRILLIC_SUPPORT
-  char cp[256];
-  translateUTF8ToBlocks(cp, str, sizeof(cp));
-  str = cp;
-#endif
   display.print(str);
 }
 
@@ -163,11 +156,6 @@ void ST7735Display::drawXbm(int x, int y, const uint8_t* bits, int w, int h) {
 }
 
 uint16_t ST7735Display::getTextWidth(const char* str) {
-#ifdef CYRILLIC_SUPPORT
-  char cp[256];
-  translateUTF8ToBlocks(cp, str, sizeof(cp));
-  str = cp;
-#endif
   int16_t x1, y1;
   uint16_t w, h;
   display.getTextBounds(str, 0, 0, &x1, &y1, &w, &h);
